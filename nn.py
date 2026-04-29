33e90a5b39f5590fb8f6c1e1445e8772b31e3a7e
menisadi <menisadi@gmail.com>
2026-04-29 22:42:35 +0300

    fixing the __call__ and adding bias

--- nn.py
+++ nn.py
@@ -1,15 +1,18 @@
 from micrograd import Value
-from random import random
+from random import uniform
 
-class Node:
+class Neuron:
     _weights: list[Value]
+    _bias: Value
+
     def __init__(self, size: int) -> None:
-        self._weights = [Value(random()) for _ in range(size)]
+        self._weights = [Value(uniform(-1, 1)) for _ in range(size)]
+        self._bias = Value(uniform(-1, 1))
         
 
     def __call__(self, inputs: list[Value]) -> Value:
-        prod = sum(x*w for x,w  in zip(self._weghts, inputs))
-        return Value(0)
+        dot_prod = sum([x * w for x, w  in zip(self._weights, inputs)], self._bias)
+        return dot_prod.tanh()
 
 class Layer:
     def __init__(self) -> None:
@@ -18,3 +21,11 @@ class Layer:
 class MLP:
     def __init__(self) -> None:
         pass
+
+if __name__=="__main__":
+    # sanity check
+    xs = [Value(i) for i in range(3)]
+    n = Neuron(3)
+    print(n._weights)
+    print(n._bias)
+    print(n(xs))
