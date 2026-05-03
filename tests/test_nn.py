cc3b7f0cc60db0f45c3a46fa796eb081ddac8036
menisadi <menisadi@gmail.com>
2026-05-03 09:12:38 +0300

    added: tests for mlp

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,12 +1,14 @@
 from src.nn import Neuron, Layer, MLP
 from src.micrograd import Value
 
+
 def test_neuron_call():
     xs = [Value(i) for i in range(3)]
     n = Neuron(3)
     result = n(xs)
     assert isinstance(result, Value)
     assert result.data >= -1 and result.data <= 1
+
 
 def test_neuron_grad():
     xs = [Value(i) for i in range(1, 4)]
@@ -16,6 +18,7 @@ def test_neuron_grad():
     assert all(x.grad != 0 for x in xs)
     assert all(p.grad != 0 for p in n.parameters())
 
+
 def test_layer_call():
     xs = [Value(i) for i in range(3)]
     inputes, outputs = 3, 4
@@ -23,6 +26,7 @@ def test_layer_call():
     out = one_layer(xs)
     assert len(out) == outputs
     assert all(r.data >= -1 and r.data <= 1 for r in out)
+
 
 def test_layer_grad():
     xs = [Value(i) for i in range(1, 4)]
@@ -34,12 +38,14 @@ def test_layer_grad():
     assert all(x.grad != 0 for x in xs)
     assert all(p.grad != 0 for p in one_layer.parameters())
 
+
 def test_mlp_call():
     xs = [Value(i) for i in range(1, 4)]
     layers_sizes = [3, 4, 2]
     mlp = MLP(layers_sizes)
     out = mlp(xs)
     assert all(r.data >= -1 and r.data <= 1 for r in out)
+
 
 def test_mlp_grad():
     xs = [Value(i) for i in range(1, 4)]
