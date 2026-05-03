088409a5008929b9915060a431f0472dbb0a8e5d
menisadi <menisadi@gmail.com>
2026-05-03 09:12:28 +0300

    feat: plotting loss and tqdm for progress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.micrograd import Value
 from src.nn import MLP, quad_loss
 from collections.abc import Callable
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 def value_example():
@@ -33,16 +35,18 @@ def train(
     step: float,
     loss: Callable[[list[Value], list[Value]], Value],
     iterations: int,
-):
-    for _ in range(iterations):
+) -> tuple[list[Value], list[float | int]]:
+    losses: list[float | int] = []
+    for _ in tqdm(range(iterations)):
         ypred = [mlp(inpt)[0] for inpt in xs]
-        losses = quad_loss(ys, ypred)
-        losses.backward()
+        current_loss = quad_loss(ys, ypred)
+        current_loss.backward()
+        losses.append(current_loss.data)
         for p in mlp.parameters():
             p.data -= p.grad * step
             p.grad = 0
     ypred = [mlp(inpt)[0] for inpt in xs]
-    losses = loss(ys, ypred)
+    current_loss = loss(ys, ypred)
     return ypred, losses
 
 
@@ -56,10 +60,12 @@ def full_example():
     ys = [Value(0.0), Value(1.0), Value(1.0), Value(0.0)]
     mlp = MLP([2, 4, 1])
 
-    ypred, losses = train(xs, ys, mlp=mlp, step=0.1, loss=quad_loss, iterations=1000)
-    print(losses.data)
+    ypred, losses = train(xs, ys, mlp=mlp, step=0.02, loss=quad_loss, iterations=10000)
+    print(losses[-1])
     print(f"True: {ys}")
     print(f"Predictions: {ypred}")
+    _ = plt.plot(range(len(losses)), losses)
+    plt.show()
 
 
 if __name__ == "__main__":
